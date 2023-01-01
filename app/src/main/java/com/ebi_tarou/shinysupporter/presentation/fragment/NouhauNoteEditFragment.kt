f7d9846249d84dfc8f187e798b527a1496cc6cfe
shota-ebata <maamaaomosiroi@gmail.com>
2023-01-01 21:03:29 +0900

    JetpackCompose導入にあたって、xmlやFragmentを除却

--- app/src/main/java/com/ebi_tarou/shinysupporter/presentation/fragment/NouhauNoteEditFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.ebi_tarou.shinysupporter.presentation.fragment
-
-import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class NouhauNoteEditFragment: Fragment() {
-
-}
