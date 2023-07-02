f0c23eeff7e93e5d20f0cdc826d4f5a985d10674
ebata-shota <maamaaomosiroi@gmail.com>
2023-07-02 22:06:07 +0900

    NouhauListViewModelのHiltアノテーションつけ忘れ

--- app/src/main/java/com/ebi_tarou/shinysupporter/presentation/viewmodel/NouhauListViewModel.kt
+++ app/src/main/java/com/ebi_tarou/shinysupporter/presentation/viewmodel/NouhauListViewModel.kt
@@ -4,15 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ebi_tarou.shinysupporter.domain.model.Nouhau
 import com.ebi_tarou.shinysupporter.domain.repository.NouhauMasterRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NouhauListViewModel(
+@HiltViewModel
+class NouhauListViewModel
+@Inject
+constructor(
     private val repository: NouhauMasterRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(NouhauListUIState(loading = true))
     val uiState: StateFlow<NouhauListUIState> = _uiState.asStateFlow()
